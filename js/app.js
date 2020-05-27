d8b4f68f65760e400f067d4382bad04560645a56
tgihan <gihanpurple@gmail.com>
2020-05-27 16:59:50 +0530

    17

--- js/app.js
+++ js/app.js
@@ -80,7 +80,7 @@ $(document).ready(function () {
 
   // Evento para whastapp
   $('#wpp').on('click', function(){
-    window.open("https://api.whatsapp.com/send?phone=51971313111&text=I'm%20interested%20in%20your%20portfolio");
+    window.open("https://api.whatsapp.com/send?phone=94773296840&text=I'm%20interested%20in%20your%20portfolio");
   })
 
   // modal
